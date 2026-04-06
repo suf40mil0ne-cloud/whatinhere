346ebe3f8758b460dd4e58471bea66b519970cdb
suf40mil0ne <suf40mil0ne@gmail.com>
2026-04-06 05:35:56 +0000

    hardcode kakao key in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   envPrefix: ["VITE_"],
+  define: {
+    "import.meta.env.VITE_KAKAO_MAP_JS_KEY": JSON.stringify("196acd86c9ca7b2a46f77dd0d90f11f1"),
+  },
   server: {
     port: 5173,
     proxy: {
